54e97435faab5256dea0b0954bed8f3466f0b154
DEEPAKTSA <deepaknagarajan.6@gmail.com>
2025-09-21 14:01:28 +0530

    Updating latest test cases

--- SampleCIProject/src/LoginTest.java
+++ SampleCIProject/src/LoginTest.java
@@ -5,6 +5,10 @@ public class LoginTest {
 		// TODO Auto-generated method stub
 
 		System.out.println("Perform login test");
+		System.out.println("Test failed");
+		System.out.println("Test success");
+	
+		
 	}
 
 }
